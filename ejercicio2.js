b607d633c1389dc9314aa82b3f844ef23676d501
Abituit <aby2507@gmail.com>
2024-01-09 15:32:16 -0600

    add funciones

---
+++ ejercicio2.js
@@ -0,0 +1,30 @@
+/**Ejercicios para esta sesion
+ * 
+ * Declara una funcion para cada problema planteado
+ * 
+ * calcular el perimetro de un circulo
+ * calcular el area de un rectangulo
+ * calcular el cuadrado de un numero
+ * calcular la conversion de grados celsius a farenheit
+ * calcular el valor del voltaje dadas la resistencia y la corriente
+ * calcular el volumen de una esfera
+ * 
+ * CONSIDERACIONES: poner en practica las mejores practicas para declarar funciones
+ * Los ejercicios se entregan tanto en funciones con nombre como funciones flecha
+ */
+
+function calcularPerimetro(nombre){
+    console.log("Hola " + nombre);
+
+}
+
+function calcularPerimetro(l){
+    return 5 + 25 + 10 + 40}
+
+/* function calcularPerimetro(l){
+    p = 5 + 25 + 10 + 40
+    return p;
+} */
+
+const calcularPerimetroFlecha = (parametro) => parametro;
+
