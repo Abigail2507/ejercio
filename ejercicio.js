e454a56b22020a94c9f8fa59b2729819ed5585a7
Abituit <aby2507@gmail.com>
2024-01-08 19:54:11 -0600

    se realiza el ejercicio

---
+++ ejercicio.js
@@ -0,0 +1,150 @@
+/** Ejercicio para esta sesion
+ * 
+ * Declarar 10 variables de cada tipo de dato sin tomar en cuenta objetos, arrays y null
+ * Pueden variar entre las palabras reservadas var, let y const
+ *      -number
+ *      -string
+ *      -boolean
+ * 
+ * Declarar variables numericas y realizar las operaciones aritmeticas basicas
+ *      -suma
+ *      -resta
+ *      -multiplicacion
+ *      -division
+ * 
+ * Declarar variables que concatenen cadenas de texto
+ *      -5 mensajes diferentes en pantalla, sin limite de palabras.
+ * 
+ * Declarar 5 variables y convertirlas a otro tipo de dato
+ * 
+ *  CONSIDERACIONES: poner en practica las mejores practicas para declaracion de variables.
+ * Imprimir los resultados en pantalla con un console.log()
+ */
+let nombreDeEsuario = "Abigail";
+console.log(nombreDeEsuario)
+
+let apeidoDeAbigail="Martinez"
+console.log(apeidoDeAbigail)
+
+let coleccionTiendasdeRopa = ["Shein", "Zara", "Bershka"]; 
+console.log(coleccionTiendasdeRopa)
+
+let serieFavorita = "La Casa de Papel";
+console.log(serieFavorita)
+
+let cantanteFavorita = "Karol G";
+console.log(cantanteFavorita)
+
+let epocaFavorita = "Navidad";
+console.log(epocaFavorita)
+
+let estacionFavorita = "Invierno";
+console.log(estacionFavorita)
+
+let hobbiesFav=["Leer", "Bailar", "Visitar pueblitos magicos"];
+console.log(hobbiesFav)
+
+let colorFavorito= "Verde Turquesa";
+console.log(colorFavorito)
+
+let pueblosMagicosvisitados= ["Puebla", "Veracruz", "Bacalar", "Tlamanalco" ]
+console.log(pueblosMagicosvisitados)
+
+let comidaFavorita = "Tacos"
+console.log(comidaFavorita)
+
+let amIAlive = true;
+console.log(amIAlive)
+
+let meGustaProgramar = true
+console.log(meGustaProgramar)
+
+let meGustaDibujar = false
+console.log(meGustaDibujar)
+
+let tengoHijos = false
+console.log(tengoHijos)
+
+let vivoEnMexico = true
+console.log(vivoEnMexico)
+
+let noMeGustaLaCebolla = true
+console.log(noMeGustaLaCebolla)
+
+let meGustaJugarVideoJuegos = true
+console.log(meGustaJugarVideoJuegos)
+
+let eresMayorDeEdad = true
+console.log (eresMayorDeEdad)
+
+let conocesBacalar = true
+console.log(conocesBacalar)
+
+let conocesVeracruz = true
+console.log(conocesVeracruz)
+
+let conocesPuebla = true
+console.log(conocesPuebla)
+
+let fechaEnCurso = 2024
+console.log(fechaEnCurso)
+
+let fechaDeMiNacimiento = 1995
+console.log(fechaDeMiNacimiento)
+
+let edadDeTuMadre = 65
+console.log(edadDeTuMadre)
+
+let edadDeTuPadre = 70
+console.log(edadDeTuPadre)
+
+let fechaQueTeDejoUnaEnseñanza = 2023
+console.log(fechaQueTeDejoUnaEnseñanza)
+
+let edadDeTuHermano = 41
+console.log(edadDeTuHermano)
+
+let fechaDeNacimientoDeTuHermano= 1982
+console.log(fechaDeNacimientoDeTuHermano)
+
+let fechaQueSeCasaronTusPadres = 1980
+console.log(fechaQueSeCasaronTusPadres)
+
+var numeroFavorito = 5
+console.log(numeroFavorito)
+
+let fechaDeTusXV = 2005
+console.log(fechaDeTusXV)
+
+
+
+let suma = fechaEnCurso + fechaQueSeCasaronTusPadres
+console.log("la suma de fechaEnCurso y fechaQueSeCasaronTusPadres"+ suma)
+
+let resta = fechaQueTeDejoUnaEnseñanza - fechaDeNacimientoDeTuHermano
+console.log("la resta de la fecha y la fecha de mi hermano"+ resta)
+
+let division = edadDeTuPadre / numeroFavorito
+console.log("division de la edad de tu padre y el numero favorito"+division)
+
+let multiplicacion = edadDeTuMadre * numeroFavorito
+console.log("multiplicación entre la edad de tu madre y el número favorito"+multiplicacion);
+
+
+let nombreCompleto = "nombreDeEsuario" + " apeidoDeAbigail"
+console.log(nombreDeEsuario +  apeidoDeAbigail)
+
+let estacionyepoca =  epocaFavorita +  estacionFavorita
+console.log(epocaFavorita +  estacionFavorita)
+
+let amoFavorita = "Amo" + " La Casa de Papel";
+console.log("Amo" + " La Casa de Papel")
+
+let amoprogramacion = "Amo" + " La Programación";
+console.log("Amo" + " La Programación")
+
+let agradecimiento = "Gracias" + " Profesores" + " y Generation";
+console.log("Gracias" + " Profesores" + " y Generation")
+
+let edadDeTuMadree = Number(edad);
+let nombreNumerico = Number(nombre);
